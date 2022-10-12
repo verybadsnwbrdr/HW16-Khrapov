3fae4f0b5a98663c4b2b456ece9233d9c51c2317
Anton Khrapov <froe054@mail.ru>
2022-10-12 23:27:01 +0700

    Final features

--- HW16-Khrapov/Sources/Scenes/ViewController.swift
+++ HW16-Khrapov/Sources/Scenes/ViewController.swift
@@ -10,6 +10,8 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Stored Properties
+    
     private var isBlack: Bool = false {
         didSet {
             self.view.backgroundColor = isBlack ? .white : .black
@@ -24,14 +26,15 @@ class ViewController: UIViewController {
     }
     
     // MARK: - Elements
-
+    
     private lazy var textField: UITextField = {
         let textField = UITextField()
-        textField.backgroundColor = .systemGray6
+        textField.backgroundColor = .systemGray2
         textField.textAlignment = .center
         textField.isSecureTextEntry = true
         textField.returnKeyType = .done
         textField.placeholder = "Enter password"
+        textField.layer.cornerRadius = 15
         return textField
     }()
     
@@ -41,27 +44,45 @@ class ViewController: UIViewController {
         return indicator
     }()
     
+    private lazy var secureButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        button.addTarget(self, action: #selector(toggleSecureButtonAndTextField), for: .touchUpInside)
+        button.tintColor = .label
+        button.contentMode = .scaleAspectFit
+        button.layer.cornerRadius = 15
+        return button
+    }()
+    
     private lazy var label: UILabel = {
         let label = UILabel()
-        label.backgroundColor = .systemGray6
+        label.backgroundColor = .systemGray2
         label.text = "Enter password and press Start"
         label.textAlignment = .center
+        label.layer.cornerRadius = 15
+        label.clipsToBounds = true
         return label
     }()
     
     private lazy var brutStartStop: UIButton = {
         let button = UIButton(type: .system)
         button.addTarget(self, action: #selector(brutActivate), for: .touchUpInside)
-        button.backgroundColor = .systemGray6
+        button.backgroundColor = .systemGray2
+        button.layer.cornerRadius = 15
         button.setTitle("Start", for: .normal)
+        button.tintColor = .label
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17)
         return button
     }()
     
-    private lazy var button: UIButton = {
+    private lazy var changeBackColorButton: UIButton = {
         let button = UIButton(type: .system)
-        button.addTarget(self, action: #selector(onBut), for: .touchUpInside)
+        button.addTarget(self, action: #selector(changeBackColor), for: .touchUpInside)
         button.setTitle("Change color", for: .normal)
-        button.backgroundColor = .systemGray6
+        button.backgroundColor = .systemGray2
+        button.layer.cornerRadius = 15
+        button.tintColor = .label
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17)
         return button
     }()
     
@@ -69,7 +90,7 @@ class ViewController: UIViewController {
         let stack = UIStackView()
         stack.axis = .vertical
         stack.alignment = .center
-        stack.spacing = 10
+        stack.spacing = 12
         return stack
     }()
     
@@ -87,9 +108,10 @@ class ViewController: UIViewController {
         stack.addArrangedSubview(label)
         stack.addArrangedSubview(textField)
         stack.addArrangedSubview(brutStartStop)
-        stack.addArrangedSubview(button)
+        stack.addArrangedSubview(changeBackColorButton)
         view.addSubview(stack)
         view.addSubview(activityIndicator)
+        view.addSubview(secureButton)
     }
     
     private func setupLayout() {
@@ -99,7 +121,7 @@ class ViewController: UIViewController {
             make.height.equalTo(50)
         }
         
-        button.snp.makeConstraints { make in
+        changeBackColorButton.snp.makeConstraints { make in
             make.left.right.height.equalTo(textField)
         }
         
@@ -116,121 +138,111 @@ class ViewController: UIViewController {
         }
         
         activityIndicator.snp.makeConstraints { make in
-            make.left.equalTo(textField.snp.left).offset(15)
+            make.right.equalTo(textField.snp.right).offset(-20)
             make.centerY.equalTo(textField)
+        }
+        
+        secureButton.snp.makeConstraints { make in
+            make.left.equalTo(textField.snp.left).offset(20)
+            make.centerY.equalTo(textField)
+            make.width.equalTo(25)
+            make.height.equalTo(20)
         }
     }
     
     // MARK: - Actions
     
-    func bruteForce(passwordToUnlock: String) {
-
-        let ALLOWED_CHARACTERS: [String] = String().printable.map { String($0) }
-        
-        var password: String = ""
-        
-        while password != passwordToUnlock {
-            guard isBrutActive else {
-                DispatchQueue.main.async { [self] in
-                    guard let password = textField.text else { return }
-                    changeLabel(with: "Your password «\(password)» not hacked!")
-                    
-                    DispatchQueue.main.async { [self] in
-                        activityIndicator.isHidden = true
-                        activityIndicator.stopAnimating()
-                    }
-//                    textField.isSecureTextEntry = false
-                }
-                return
-            }
-            
-            DispatchQueue.main.async { [self] in
-                activityIndicator.isHidden = false
-                activityIndicator.startAnimating()
-            }
-            
-            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
-            print(password)
-            changeLabel(with: password)
-        }
-        print(password)
-        changeLabel(with: "Your password «\(password)»")
-        
-        DispatchQueue.main.async { [self] in
-            isBrutActive.toggle()
-            textField.isSecureTextEntry = false
-            activityIndicator.isHidden = true
-            activityIndicator.stopAnimating()
-        }
-
-    }
-    
-    private func changeLabel(with text: String) {
-        DispatchQueue.main.async {
-            self.label.text = text
+    private func toggleSecureButtonImage() {
+        if secureButton.currentImage == UIImage(systemName: "eye.slash") {
+            secureButton.setImage(UIImage(systemName: "eye"), for: .normal)
+        } else {
+            secureButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
         }
     }
     
-    @objc private func onBut() {
+    private func brutStopAndChangeLabel(with text: String) {
+        label.text = text
+        isBrutActive.toggle()
+        activityIndicator.isHidden = true
+        activityIndicator.stopAnimating()
+    }
+    
+    // MARK: - Buttons Actions
+    
+    @objc private func changeBackColor() {
         isBlack.toggle()
     }
     
     @objc private func brutActivate() {
-        guard let password = textField.text else { return }
-        
-        guard password != "" else {
-            changeLabel(with: "Please, enter password")
-            return
-        }
-        
+        guard let password = textField.text, password != "" else { return label.text = "Please, enter password" }
         isBrutActive.toggle()
         
         let brutQueue = DispatchQueue(label: "brutQueue", qos: .background, attributes: .concurrent)
-        brutQueue.async {
-            self.bruteForce(passwordToUnlock: password)
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// MARK: - BruteForce
-
-func indexOf(character: Character, _ array: [String]) -> Int {
-    return array.firstIndex(of: String(character))!
-}
-
-func characterAt(index: Int, _ array: [String]) -> Character {
-    return index < array.count ? Character(array[index]) : Character("")
-}
-
-func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
-    var str: String = string
-
-    if str.count <= 0 {
-        str.append(characterAt(index: 0, array))
-    }
-    else {
-        str.replace(at: str.count - 1,
-                    with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
-
-        if indexOf(character: str.last!, array) == 0 {
-            str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+        brutQueue.async { [self] in
+            bruteForce(passwordToUnlock: password)
         }
     }
     
-    return str
+    @objc private func toggleSecureButtonAndTextField() {
+        toggleSecureButtonImage()
+        textField.isSecureTextEntry.toggle()
+    }
 }
+
+// MARK: - BruteForce Extension
+
+extension ViewController {
+    private func bruteForce(passwordToUnlock: String) {
+        let ALLOWED_CHARACTERS: [String] = String().printable.map { String($0) }
+        var password: String = ""
+        
+        while password != passwordToUnlock {
+            if !isBrutActive {
+                DispatchQueue.main.async { [self] in
+                    brutStopAndChangeLabel(with: "Your password «\(passwordToUnlock)» not hacked!")
+                }
+                return
+            }
+            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
+            DispatchQueue.main.async { [self] in
+                activityIndicator.isHidden = false
+                activityIndicator.startAnimating()
+                label.text = password
+            }
+        }
+        
+        DispatchQueue.main.async { [self] in
+            brutStopAndChangeLabel(with: "Your password «\(password)»")
+            if textField.isSecureTextEntry {
+                toggleSecureButtonAndTextField()
+            }
+        }
+    }
+    
+    private func indexOf(character: Character, _ array: [String]) -> Int {
+        return array.firstIndex(of: String(character))!
+    }
+    
+    private func characterAt(index: Int, _ array: [String]) -> Character {
+        return index < array.count ? Character(array[index]) : Character("")
+    }
+    
+    private func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
+        var str: String = string
+        
+        if str.count <= 0 {
+            str.append(characterAt(index: 0, array))
+        } else {
+            str.replace(at: str.count - 1,
+                        with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
+            if indexOf(character: str.last!, array) == 0 {
+                str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+            }
+        }
+        return str
+    }
+}
+
 
 
 
